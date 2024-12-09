17ed1da85a24d64f257b25ba0ae5525d65d2b444
cwt-git <codewithtanuj@outlook.com>
2024-12-09 18:19:09 +0530

    Check if number is prime or not

---
+++ 01_Basics/isprime.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class isprime {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt();
+
+        if (n <= 1) {
+            System.out.println(n + " is neither composite nor prime.");
+        } else {
+            boolean isPrime = true;
+            for (int i = 2; i <= Math.sqrt(n); i++) {
+                if (n % i == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+            if (isPrime) {
+                System.out.println(n + " is prime.");
+            } else {
+                System.out.println(n + " is composite.");
+            }
+        }
+        sc.close();
+    }
+}
