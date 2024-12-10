73efa875afb94e52f1fac0a731292ba575dde3c8
cwt-git <codewithtanuj@outlook.com>
2024-12-10 07:18:18 +0530

    while and for loop (Basics Questions)

---
+++ 01_Basics/While_loop/countDigits.java
@@ -0,0 +1,20 @@
+package While_loop;
+
+import java.util.Scanner;
+
+public class countDigits {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number = sc.nextInt();
+
+        int digits = 0;
+
+        while (number != 0) {
+            number /= 10;
+            digits++;
+        }
+        System.out.println("The number of digits is: " + digits);
+        sc.close();
+    }
+}
