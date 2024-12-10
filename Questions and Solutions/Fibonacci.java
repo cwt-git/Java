4fd0d818359a8fcb644834870c4d47b7b086e376
cwt-git <codewithtanuj@outlook.com>
2024-12-10 09:40:54 +0530

    Fibonacci

---
+++ Questions and Solutions/Fibonacci.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Fibonacci {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a Number: ");
+        int n = sc.nextInt();
+        sc.close();
+
+        int a = 0, b = 1;
+        System.out.println(a);
+        System.out.println(b);
+        for (int i = 2; i <= n; i++) {
+            int c = a + b;
+            System.out.println(c);
+            a = b;
+            b = c;
+        }
+    }
+}
