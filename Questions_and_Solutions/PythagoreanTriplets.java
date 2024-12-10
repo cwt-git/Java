778b2242ad16e3ceaf5bec542a036c6c666129e5
cwt-git <codewithtanuj@outlook.com>
2024-12-10 13:41:58 +0530

    PythagoreanTriplets

---
+++ Questions_and_Solutions/PythagoreanTriplets.java
@@ -0,0 +1,33 @@
+package Questions_and_Solutions;
+
+import java.util.Scanner;
+
+public class PythagoreanTriplets {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter three numbers: ");
+        int n1 = sc.nextInt();
+        int n2 = sc.nextInt();
+        int n3 = sc.nextInt();
+        sc.close();
+
+        int max = n1;
+        if (n2 >= max) {
+            max = n2;
+        }
+        if (n3 >= max) {
+            max = n3;
+        }
+
+        if (max == n1) {
+            boolean flag = ((n2 * n2) + (n3 * n3) == (n1 * n1));
+            System.out.println(flag);
+        } else if (max == n2) {
+            boolean flag = ((n1 * n1) + (n3 * n3) == (n2 * n2));
+            System.out.println(flag);
+        } else {
+            boolean flag = ((n1 * n1) + (n2 * n2) == (n3 * n3));
+            System.out.println(flag);
+        }
+    }
+}
